8f8d6b99d3bfb9d2359630ca264d1732210bbedb
Jahel Chacon <jahelchacon@gmail.com>
2021-01-31 16:39:34 -0600

    Update 1.0.6
    Se crean las pags de proveedores
    - marcas
    - Productos
    - Proveedores

--- res-web2/src/componentes/Proveedores/Proveedores.js
+++ res-web2/src/componentes/Proveedores/Proveedores.js
@@ -1,9 +1,34 @@
 import React from "react";
+import TemplateCard from "../Compartidos/TemplateCard";
+import { Container, Row } from "react-bootstrap";
+import marcas from "../../img/Proveedores/marcas.png"
+import productos from "../../img/Proveedores/productos.png"
+import proveedores from "../../img/Proveedores/proveedores.png"
 
 export default function Proveedores(){
     return(
-        <div>
+        <Container> 
             <h2>Proveedores</h2>
-        </div>
+            <Row>
+                <TemplateCard
+                    titulo={"Marcas"}
+                    img={marcas}
+                    alt={"Marcas"}
+                    url={"/marcas"}
+                ></TemplateCard>
+                <TemplateCard
+                    titulo={"Productos"}
+                    img={productos}
+                    alt={"Productos"}
+                    url={"/productos"}
+                ></TemplateCard>
+                <TemplateCard
+                    titulo={"Proveedores"}
+                    img={proveedores}
+                    alt={"Proveedores"}
+                    url={"/proveedores-2"}
+                ></TemplateCard>
+            </Row>
+        </Container>
     )
 }
